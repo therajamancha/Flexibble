d12d190a04b25a7a127b2636cee4354eec9ba577
Raj Amancha <raj.amancha@propelius.tech>
2023-07-13 17:30:03 +0530

    ...

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   },
   experimental: {
     serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 };
 
